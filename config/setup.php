d067308a7635dc1bf6d2f4c78bf0a3fdf754c190
Salvatore <>
2025-05-26 17:28:24 +0200

    Saving

--- config/setup.php
+++ config/setup.php
@@ -26,7 +26,7 @@
 			'default_language' => 'it',
 			'languages' => array(),
 			'menu_depth' => 2,
-			'font' => ''
+			'font' => 'https://use.typekit.net/gbi0ekt.css'
 		],
         'Shop' => [
             'vatIncuded' => true, //se true i prezzi inseriti nel pannello sono comprensivi di IVA
